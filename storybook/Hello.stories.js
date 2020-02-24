30830710250f0973550da78275c55aa3fc17fa2a
ryan3780 <45477679+ryan3780@users.noreply.github.com>
2020-02-24 19:46:52 +0900

    Delete Hello.stories.js

--- storybook/Hello.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import IconHoverUp from "./IconHoverUp";
-
-export default {
-  title: "components|basic/UP", // 스토리북에서 보여질 그룹과 경로를 명시
-  component: IconHoverUp // 어떤 컴포넌트를 문서화 할지 명시
-};
-
-export const iconHoverUp = () => <IconHoverUp />;
